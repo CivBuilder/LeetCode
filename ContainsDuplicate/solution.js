ffcb3aa24661b78b84925884da69d5a6ede76953
Chris Catechis <catecc1@unlv.nevada.edu>
2025-08-11 14:54:01 -0700

    Create solution.js

---
+++ ContainsDuplicate/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let map = new Map();
+
+    for (const num of nums) {
+        if (map.has(num)) {
+            return true;
+        }
+        map.set(num, true);
+    }
+
+    return false;
+};
