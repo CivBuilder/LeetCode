1a45ec9fadbbba0071fb5853bd966100c6f4a1d4
Chris Catechis <catecc1@unlv.nevada.edu>
2025-08-19 21:05:50 -0700

    ValidParentheses: algorithm planning
    One solution, optimal, leveraging a map and stack. I did at one point look at writing a solution where I pushed opening brackets onto the stack, then when I get to a closing bracket converting both to integers and checking the ASCII values but not every type of parentheses are next to each other on the ASCII table.

---
+++ ValidParentheses/AlgorithmPlanning.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    // solution 0
+    // time: O(n)
+    // space: O(n)
+    // create map of closing brackets to open brackets
+    // go through string
+    // if open bracket
+    // push onto stack
+    // else
+    // see if top of stack is corresponding opening bracket (via map)
+    // if not, return false
+    // return true
+};
