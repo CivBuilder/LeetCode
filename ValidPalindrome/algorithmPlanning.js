549b10fcb4079a643ebd28784e962c8cdb707995
Chris Catechis <catecc1@unlv.nevada.edu>
2025-08-20 08:25:02 -0700

    ValidPalindrome algorithm planning
    Both have a time/space complexity of O(n). I can (and should) later implement a solution without leveraging the tools of the language and write my own alphanumeric checker; but I do wonder if it makes a big difference to not just leverage what the language gives me?

---
+++ ValidPalindrome/algorithmPlanning.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+    // solution 0
+    // time: O(n)
+    // space: O(n)
+    // lowercase all letters in the string
+    // remove all non ASCII letter characters from the string
+    // go through the string with two pointers, if the value in the string at each aren't the same, return false
+    // return true
+
+    // solution 0
+    // time: O(n)
+    // space: O(n)
+    // lowercase all letters in the string
+    // remove all non alpha numeric characters from the string
+    // if string doesn't equal the reversed string, return false (else true)
+};
